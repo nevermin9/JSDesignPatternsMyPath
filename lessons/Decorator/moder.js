8ab250d2eb71eb801f85eb4b4e1f096b6f392dff
Platoniux <platoniux@deversin.com>
2023-08-09 08:49:43 +0200

    add modern decorator design pattern realization

---
+++ lessons/Decorator/moder.js
@@ -0,0 +1,42 @@
+class Bird
+{
+  constructor(age=0)
+  {
+    this.age = age;
+  }
+
+  fly()
+  {
+    return this.age < 10 ? 'flying' : 'too old';
+  }
+}
+
+class Lizard
+{
+  constructor(age=0)
+  {
+    this.age = age;
+  }
+
+  crawl()
+  {
+    return this.age > 1 ? 'crawling' : 'too young';
+  }
+}
+
+class Dragon
+{
+  constructor(age=0)
+  {
+    this.bird = new Bird(age)
+    this.lizard = new Lizard(age)
+  }
+
+  fly() {
+    return this.bird.fly()
+  }
+
+  crawl() {
+    return this.lizard.crawl()
+  }
+}
